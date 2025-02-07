7a5bde758f596f910de2e058139d8cfe09d5cf3d
CosmosPoulet <ethandu84@gmail.com>
2025-02-07 17:13:33 +0100

    Get Specific Verset

---
+++ web/script.js
@@ -0,0 +1,40 @@
+async function getVerse() {
+    const livre = document.getElementById("livre").value;
+    if(!livre) {
+        alert("Veuillez entrer un livre, mon enfant !");
+        return
+    }
+    const chapitre = document.getElementById("chapitre").value;
+    if(!chapitre) {
+        alert("Veuillez entrer un chaptitre, mon enfant !");
+        return
+    }
+    const verset = document.getElementById("verset").value;
+    if(!verset) {
+        alert("Veuillez entrer un verset, mon enfant !");
+        return
+    }
+
+    const apiUrl = getBibleVerseUrl(livre, chapitre, verset)
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.text) {
+            document.getElementById("verse-result").innerHTML = '<p>Verset: '+ data.text + '</p>';
+        } else {
+            document.getElementById("verse-result").innerHTML = "<p>Attention: Que ton cœur ne se trouble point, ô chercheur ! Car l'absence de résultat n'est point une fin, mais un nouveau commencement sur le chemin de la découvert</p>";
+        }
+    } catch(error) {
+        document.getElementById("verse-result").innertHTML = '<p>Erreur lors de la récupération du verset.</p>'
+    }
+}
+
+function getBibleVerseUrl(livre, chapitre, verset) {
+    return "https://bible-api.com/" + livre + chapitre + ":" + verset
+}
+
+module.exports = {
+    getBibleVerseUrl
+}
